6f5eee5f7e7d15fb8580441993eda0b1637fb40e
vlado-manov <vladimir.manov.bg@gmail.com>
2024-12-10 20:30:37 +0200

    Moving to typescript and set a basic FE structure

---
+++ frontend/src/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import Home from "./Views/Home";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<Home />} />
+    </Route>
+  )
+);
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
